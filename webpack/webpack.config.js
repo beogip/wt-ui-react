75b694f8cbe6ed8e8ddcbeea23da686606dd4563
Pablo Alice <pabloalice95@gmail.com>
2018-10-04 11:21:22 -0300

    update webpack config to match with storybook webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,10 +1,17 @@
-const path = require('path');
+const path = require('path')
+const precss = require('precss');
+const autoprefixer = require('autoprefixer');
+
+const appSrc = path.resolve(__dirname, "../src")
 
 module.exports = {
   resolve: {
-    modules: [path.resolve(__dirname, '../src'), 'node_modules'],
+    modules: [appSrc, 'node_modules'],
     extensions: ['.js', '.jsx'],
   },
+  plugins: [
+    // your custom plugins
+  ],
   module: {
     rules: [
       {
@@ -13,6 +20,48 @@ module.exports = {
         use: {
           loader: 'babel-loader',
         },
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: "css-loader",
+            options: {
+              root: appSrc,
+              camelCase: true,
+              modules: true,
+              localIdentName: '[name]__[local]--[hash:base64:5]',
+              importLoaders: 2,
+            }
+        },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+                plugins: [
+                  autoprefixer,
+                  precss,
+                ]
+            }
+          },
+          "sass-loader"
+        ],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'img/',
+              publicPath: 'img/'
+            }
+          }
+        ]
       },
     ],
   },
