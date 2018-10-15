e96824eb398cef434e74c4713c8565e92071a00b
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2018-10-15 10:36:51 +0200

    chore(dependencies): Add react-bootstrap

--- test/green.spec.jsx
+++ test/green.spec.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import sinon from 'sinon';
 
 import Button from '../src/components/Button';
 
-it('Renders <Button /> component', () => {
-  expect(true).toBe(true);
-});
-
 describe('<Button />', () => {
-  it('Renders <Button /> component', () => {
+  const basicProps = {
+
+  };
+  const fake = sinon.fake();
+  it('Renders <Button /> component with <button ...>', () => {
     const wrapper = shallow(<Button />);
     expect(wrapper.exists()).toBe(true);
+  });
+  it('Renders <Button /> component <a ...>', () => {
+    const wrapper = shallow(<Button href='#'/>);
+    expect(wrapper.exists()).toBe(true);
+  });
+  it('Must call function onClick', () => {
+    const wrapper = shallow(<Button onClick={fake} />);
+    wrapper.simulate('click');
+    expect(fake.callCount).toBe(1);
   });
 });
