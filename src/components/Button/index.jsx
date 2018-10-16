bbbea1893c6cf2929c0ea64407f8a3d1ba62e659
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2018-10-16 15:58:41 +0200

    feat(Button): Add first version

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,20 +1,55 @@
 /* @flow */
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import classNames from 'classnames';
 import styles from './styles.scss';
 
 type PropsType = {
-  /** This is an example of prop description */
-  style?: {},
-  onClick: string => void,
-  children?: React$Node
+  active?: boolean,
+  block?: boolean,
+  className?: string,
+  children: React$Node,
+  disabled?: boolean,
+  href?: string,
+  kind: 'primary' | 'accent' | 'secondary' | 'light' | 'link',
+  onClick?: string => void,
+  outlined: boolean,
+  size?: 'small' | 'large',
+  type?: 'button' | 'reset' | 'submit'
 };
 
-const Button = (props: PropsType) => (
-  <button className={styles.test} {...props} type="button" />
-);
+const WTButton = (props: PropsType) => {
+  const {
+    active, disabled, size, kind, children, className, block, outlined, ...others
+  } = props;
 
-Button.defaultProps = {
-  style: { color: 'green' },
+  const outlineClass = `outline-${kind}`;
+  const componentClass = classNames(
+    styles.default,
+    styles[kind],
+    styles[size],
+    {
+      [styles.active]: active,
+      [styles.block]: block,
+      [styles.disabled]: disabled,
+      [styles[outlineClass]]: outlined,
+    },
+    className,
+  );
+
+  const bsProps = {
+    bsClass: componentClass,
+  };
+  return (
+    <Button {...bsProps} {...others}>
+      {children}
+    </Button>
+  );
 };
 
-export default Button;
+WTButton.defaultProps = {
+  kind: 'primary',
+  type: 'button',
+};
+
+export default WTButton;
