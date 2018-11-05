73f300ffd03aed3055dbbfa0ba5756fb366b7932
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2018-11-05 19:30:16 +0100

    test(Layout): Add Flow

--- test/Layout/Layout.spec.jsx
+++ test/Layout/Layout.spec.jsx
@@ -1,3 +1,4 @@
+/* @flow */
 import React from 'react';
 import { shallow } from 'enzyme';
 import sinon from 'sinon';
@@ -9,25 +10,21 @@ import Row from '../../src/components/Layout/Row';
 describe('Layout', () => {
   describe('<Container />', () => {
     it('Renders <Container />', () => {
-      const wrapper = shallow(<Container />);
+      const wrapper = shallow(<Container><Row>Test</Row></Container>);
       expect(wrapper.exists()).toBe(true);
     });
   });
 
   describe('<Row />', () => {
-    it('Renders <Container />' , () => {
-      const wrapper = shallow(<Row />);
+    it('Renders <Row />' , () => {
+      const wrapper = shallow(<Row>Test</Row>);
       expect(wrapper.exists()).toBe(true);
     });
   });
 
   describe('<Col />', () => {
-    const basicProps = {
-
-    };
-    const fake = sinon.fake();
     it('Renders <Col />' , () => {
-      const wrapper = shallow(<Col />);
+      const wrapper = shallow(<Col>Test</Col>);
       expect(wrapper.exists()).toBe(true);
     });
   });
