0ed1463df0524810789861878664ca84ad19d49f
Pablo Alice <pabloalice95@gmail.com>
2018-11-13 14:10:04 -0300

    feat(Button story, ToggleButton, ToggleButtonGroup): add new componentes and update the buttton stor

--- stories/button.stories.jsx
+++ stories/button.stories.jsx
@@ -1,22 +1,67 @@
-/* eslint-disable import/no-extraneous-dependencies */
+// @flow
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import '@windingtree/wt-ui/dist/styles.css';
+import {
+  withKnobs, boolean, select, text,
+} from '@storybook/addon-knobs/react';
 import Button from 'components/Button';
+import ToggleButton from 'components/ToggleButton';
 
-storiesOf('Button', module)
-  .add('Primary', () => <Button onClick={action('clicked')}>Primary Button</Button>)
-  .add('Accent', () => <Button onClick={action('clicked')} variant="accent">Primary Button</Button>)
-  .add('Light', () => <Button onClick={action('clicked')} variant="light">Light Button</Button>)
-  .add('Primary outlined', () => <Button onClick={action('clicked')} outlined>Primary Button</Button>)
-  .add('Accent outlined', () => <Button onClick={action('clicked')} variant="accent" outlined>Primary Button</Button>)
-  .add('Light outlined', () => <Button onClick={action('clicked')} variant="light" outlined>Light Button</Button>)
-  .add('Active', () => <Button onClick={action('clicked')} active>Active Button</Button>)
-  .add('Block', () => <Button onClick={action('clicked')} block>Block Button</Button>)
-  .add('Disabled', () => <Button onClick={action('clicked')} disabled>Disabled Button</Button>)
-  .add('Link', () => <Button href="#">Link Button</Button>)
-  .add('Small', () => <Button onClick={action('clicked')} size="sm">Small Button</Button>)
-  .add('Large', () => <Button onClick={action('clicked')} size="lg">Large Button</Button>)
-  .add('All', () => <Button href="#" active block disabled size="lg">All Button</Button>);
+import '@windingtree/wt-ui/dist/styles.css';
+import ToggleButtonGroup from 'components/ToggleButtonGroup';
+
+const variantOptions = {
+  primary: 'primary',
+  secondary: 'secondary',
+  accent: 'accent',
+  light: 'light',
+  dark: 'dark',
+  danger: 'danger',
+  warning: 'warning',
+  success: 'success',
+  info: 'info',
+};
+const stories = storiesOf('Button', module);
+stories.addDecorator(withKnobs);
+
+stories
+  .add('Button', () => (
+    <Button
+      onClick={action('clicked')}
+      variant={select('variant', variantOptions, 'primary')}
+      active={boolean('active', false)}
+      block={boolean('block', false)}
+      disabled={boolean('disabled', false)}
+      outlined={boolean('outlined', false)}
+      className={text('className', '')}
+      size={select('size', [undefined, 'sm', 'lg'], undefined)}
+    >
+      {text('children', 'WTButton')}
+    </Button>
+  ))
+  .add('ToggleButton', () => (
+    <ToggleButtonGroup type="checkbox" defaultValue="test">
+      <ToggleButton
+        value={{ test: 'test' }}
+        onClick={action('clicked')}
+        variant={select('variant', variantOptions, 'primary')}
+        active={boolean('active', false)}
+        block={boolean('block', false)}
+        disabled={boolean('disabled', false)}
+        outlined={boolean('outlined', false)}
+        className={text('className', '')}
+        size={select('size', [undefined, 'sm', 'lg'], undefined)}
+      >
+        {text('children', 'Customizable Button')}
+      </ToggleButton>
+      <ToggleButton
+        value="test"
+        onClick={action('fixedButton clicked')}
+        variant="accent"
+      >
+        Fixed Button
+      </ToggleButton>
+    </ToggleButtonGroup>
+  ));
