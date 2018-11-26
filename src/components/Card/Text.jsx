36d141b1d2a4a7e4d0ec8997a5e7da2b9f6ff3ea
beogip <beogip@gmail.com>
2018-11-26 09:57:36 -0300

    feat(Card): add missing components
    added missing components link, subtitle, text and title

---
+++ src/components/Card/Text.jsx
@@ -0,0 +1,20 @@
+/* @flow */
+import React from 'react';
+import BSCard from 'react-bootstrap/lib/Card';
+
+
+type PropsType = {
+  /**
+   * You can use a custom element type for this component.
+   */
+  as: React$Element<*> | string
+};
+
+const WTText = (props: PropsType) => (<BSCard.Text {...props} />);
+
+WTText.defaultProps = {
+  as: 'p',
+};
+
+
+export default WTText;
