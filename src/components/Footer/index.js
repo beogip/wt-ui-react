b63063cbdcfacfb7e76ddbda1bb9b85a77d98fbf
Pablo Alice <pabloalice95@gmail.com>
2018-11-30 22:22:42 -0300

    fix(Footer): added Footer export and added Footer subcomponent as Footer properties

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,17 @@
 /* @flow */
 import React from 'react';
+import mapDisplayNames from 'utils/componentNames';
+
 import IconList from './IconList';
 import Copyright from './Copyright';
 import Logo from './Logo';
+import About from './About';
+import Comunity from './Comunity';
+import Contacts from './Contacts';
+import Developers from './Developers';
+import LifToken from './LifToken';
+import Solutions from './Solutions';
+
 import './styles.scss';
 
 type PropsType = {
@@ -35,5 +44,14 @@ const WTFooter = (props: PropsType) => {
     </footer>
   );
 };
+
+WTFooter.About = About;
+WTFooter.Comunity = Comunity;
+WTFooter.Contacts = Contacts;
+WTFooter.Developers = Developers;
+WTFooter.LifToken = LifToken;
+WTFooter.Solutions = Solutions;
+
+mapDisplayNames(WTFooter);
 
 export default WTFooter;
