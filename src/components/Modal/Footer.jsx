e554df917be3d7007b3454ff55e15bccc8c47287
beogip <beogip@gmail.com>
2018-12-01 12:36:51 -0300

    feat(ModalFooter): add modal footer component

---
+++ src/components/Modal/Footer.jsx
@@ -0,0 +1,20 @@
+/* @flow */
+import React from 'react';
+import ModalFooter from 'react-bootstrap/lib/ModalFooter';
+
+
+type PropsType = {
+  /**
+   * You can use a custom element type for this component.
+   */
+  as: React$Element<*> | string
+};
+
+const WTFooter = (props: PropsType) => (<ModalFooter {...props} />);
+
+WTFooter.defaultProps = {
+  as: 'div',
+};
+
+
+export default WTFooter;
